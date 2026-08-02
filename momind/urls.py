e54a2e4e2f834b0fe8942d8720d3b1efcc428508
venujaekmith <venuja.ekmith@gmail.com>
2026-08-02 18:27:09 +0530

    Fix static file serving in development

--- momind/urls.py
+++ momind/urls.py
@@ -36,6 +36,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
